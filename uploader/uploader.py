3eb5eb33177baeabe9b18e668193c1073ab9128a
DW <damian.wloch@gmail.com>
2021-06-02 12:23:21 +0000

    putting in server side validation for CSV uploads and trimming comments

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -15,7 +15,6 @@ landing_page = 'upload.html'
 
 @app.route('/')
 def upload():
-    #TODO: put validation here to print sane messages if the file doesn't exist
     try: 
         timestamp = stat(sheet_dir + "/" + output_sheet).st_mtime
         date = str(datetime.fromtimestamp(timestamp)).split('.')[0]
@@ -23,18 +22,19 @@ def upload():
     except FileNotFoundError:
         return render_template(landing_page, output_message=f"No spreadsheet found")
 
-#    timestamp = stat(sheet_dir + "/" + sheet_name).st_mtime
-#    date = str(datetime.fromtimestamp(timestamp)).split('.')[0]
-#    return render_template(landing_page, output_message=f"Last processed: {date}")
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         if f.filename == "":  return render_template('error.html', title="Upload Error", message="No file attached")
-        #f.save(sheet_dir + secure_filename(f.filename))
-        #TODO: put validation in 
-        f.save(sheet_dir + "/" + input_csv)
+
+        for line in f:
+            if len(str(line).split(',')) != 2:
+                del f
+                return render_template('error.html')
+
+        f.save(sheet_dir + "/" + secure_filename(input_csv))
         return render_template('success.html')
 
 if __name__ == '__main__':
