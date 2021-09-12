2c6a6e6076355b49684c0745a3d163bf621af179
mc587 <59673462+mc587@users.noreply.github.com>
2021-09-12 15:20:44 -0400

    Update vvs_api.py

--- analysis/vvs_api.py
+++ analysis/vvs_api.py
@@ -4,26 +4,22 @@ import pandas
 import os
 import os.path
 import json
+import time
 
 app = Flask(__name__)
 api = Api(app)
 
-# Turn excel sheet to pandas data fram
-excel_data_fragment = pandas.read_excel('/opt/vvs/diamond_sheet.xlsx', index_col=False)
-# turn data frame into excel 
-vvs_output_json = excel_data_fragment.to_json(orient="records")
-# Pretty print the json
-parsed = json.loads(vvs_output_json)
-
-
-# Class for API and return the pretty print API above
-class VvsResults(Resource):
-    def get(self):
-        return parsed
-
-# define API URL
-api.add_resource(VvsResults, "/vvsapi")
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+while True:
+    try:
+        excel_data_fragment = pandas.read_excel('/opt/vvs/diamond_sheet.xlsx', index_col=False)
+        vvs_output_json = excel_data_fragment.to_json(orient="records")
+        parsed = json.loads(vvs_output_json)
+        class VvsResults(Resource):
+            def get(self):
+                return parsed
+        api.add_resource(VvsResults, "/vvsapi")
+        if __name__ == "__main__":
+            app.run(debug=True, host='0.0.0.0')
+    except ValueError as err:
+        time.sleep(60)
+        continue
